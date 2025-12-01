5ac4374089775b29905770e08705700d26233572
Gabriela <gabriela.moreira@Unifesp.br>
2025-11-30 23:01:43 -0300

    teste

--- in_stock/app/pages/views.py
+++ in_stock/app/pages/views.py
@@ -17,6 +17,8 @@ def dashboard_view(request):
     if not request.user.is_authenticated:
         return render(request, "errors/401.html")
 
+    print("mensagem")
+
     context = {
         "total_products": 120,
         "total_suppliers": 15,
