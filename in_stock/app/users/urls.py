23d3de10cf199f5422a2e57ac511a0a101ce62fb
Amanda <nlinoamanda@gmail.com>
2025-12-03 21:17:18 -0300

    verificando formatacao urls.py users

--- in_stock/app/users/urls.py
+++ in_stock/app/users/urls.py
@@ -8,37 +8,37 @@ from . import views
 urlpatterns = [
     # Lista todos os usuários (GET) e cria um novo usuário (POST)
     path("", views.UserListCreateView.as_view(), name="user-list-create"),
-    
+
     # Recupera um usuário específico (GET), atualiza (PUT/PATCH)
     path("<int:id_user>/", views.UserDetailView.as_view(), name="user-detail"),
-    
+
     # Usa uma Template Genérica para uma página estática
     path(
         "create/",
         TemplateView.as_view(template_name="users/create.html"),
         name="user-create",
     ),
-    
+
     path("register/", views.RegisterCreate.as_view(), name="user-register"),
 
     # Rota de Login
     path(
-        "login/", 
-        auth_views.LoginView.as_view(template_name="login.html"), 
+        "login/",
+        auth_views.LoginView.as_view(template_name="login.html"),
         name="login"
     ),
 
     # Rota de Logout
     path(
-        "logout/", 
-        auth_views.LogoutView.as_view(), 
+        "logout/",
+        auth_views.LogoutView.as_view(),
         name="logout"
     ),
 
     # Rota do MENU VERTICAL
     path(
-        "menu/", 
-        TemplateView.as_view(template_name="MenuVertical.html"), 
+        "menu/",
+        TemplateView.as_view(template_name="MenuVertical.html"),
         name="MenuVertical"
     ),
 ]
