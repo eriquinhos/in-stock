14070ec2892a58a8c1ada9d687370e992a232e30
Amanda <nlinoamanda@gmail.com>
2025-12-07 17:14:25 -0300

    subindo tela de produtos formatada

--- in_stock/app/products/urls.py
+++ in_stock/app/products/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     # O 'name' aqui deve ser igual ao que está no {% url %}
-    path("pages/GestaoProdutos.html", views.ProductListView.as_view(), name="GestaoProdutos"),
+    path(
+        "pages/GestaoProdutos.html",
+        views.ProductListView.as_view(),
+        name="GestaoProdutos",
+    ),
 ]
