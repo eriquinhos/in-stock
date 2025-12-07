4ed0649373291a4d62e1f346a9e88f5170ff4302
Amanda <nlinoamanda@gmail.com>
2025-12-07 13:08:17 -0300

    verificando isort urls.py e setting.py

--- in_stock/app/pages/urls.py
+++ in_stock/app/pages/urls.py
@@ -1,17 +1,16 @@
 from django.urls import path
 
+# AQUI ESTAVA O ERRO: Organize de A a Z (Category vem antes de Product)
 from in_stock.app.products.views import (
-    ProductListCreateView,
-    ProductUpdateView,
-    ProductDeleteView,
+    CategoryDeleteView,
     CategoryListCreateView,
     CategoryUpdateView,
-    CategoryDeleteView
+    ProductDeleteView,
+    ProductListCreateView,
+    ProductUpdateView,
 )
 
 from . import views
-
-
 
 urlpatterns = [
     path("", views.home, name="home"),
@@ -78,17 +77,15 @@ urlpatterns = [
     ),
 
     # ============================================
-    # GESTÃO DE PRODUTOS (Usando os imports novos)
+    # GESTÃO DE PRODUTOS
     # ============================================
-    # Note o uso de .as_view() porque são Classes
-    
     path("gestao/produtos/", ProductListCreateView.as_view(), name="product-list-create"),
     path("gestao/produtos/<int:id_product>/editar/", ProductUpdateView.as_view(), name="product-update"),
     path("gestao/produtos/<int:id_product>/deletar/", ProductDeleteView.as_view(), name="product-delete"),
+
     # ============================================
     # GESTÃO DE CATEGORIAS
     # ============================================
-    
     path("gestao/categorias/", CategoryListCreateView.as_view(), name="category-list-create"),
     path("gestao/categorias/<int:id_category>/editar/", CategoryUpdateView.as_view(), name="category-update"),
     path("gestao/categorias/<int:id_category>/deletar/", CategoryDeleteView.as_view(), name="category-delete"),
