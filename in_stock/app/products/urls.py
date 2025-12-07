310e2e38fabe36becb05ca44ee893adbb2be15c9
Amanda <nlinoamanda@gmail.com>
2025-12-07 15:32:48 -0300

    formatando isort arquivo urls

--- in_stock/app/products/urls.py
+++ in_stock/app/products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     # O 'name' aqui deve ser igual ao que está no {% url %}
-    path('produtos/', views.minha_view_de_produtos, name='GestaoProdutos'), 
+    path("produtos/", views.minha_view_de_produtos, name="GestaoProdutos"),
 ]
