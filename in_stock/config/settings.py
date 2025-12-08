8b9aa9e0b39d4babe391a0e51c40472ab529f008
eriquinhos <ericvictorsilva.es@gmail.com>
2025-12-08 17:35:29 -0300

    email settings

--- in_stock/config/settings.py
+++ in_stock/config/settings.py
@@ -62,6 +62,14 @@ MIDDLEWARE = [
 ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.zoho.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
+DEFAULT_FROM_EMAIL = 'admin@instock.app.br'
+SERVER_EMAIL = 'admin@instock.app.br'
 
 ROOT_URLCONF = "in_stock.config.urls"
 
